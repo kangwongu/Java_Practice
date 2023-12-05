b503854e15b27f79b76ea3e99e124f49922d70c9
kangwongu <kwg0527@naver.com>
2023-12-05 16:44:44 +0900

    연산자

---
+++ java-start/src/operator/OperatorEx2.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorEx2 {
+    public static void main(String[] args) {
+        double num1 = 1.5, num2 = 2.5, num3 = 3.5;
+        double sum = num1 + num2 + num3;
+        double average = sum/3;
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+    }
+}
